514cd1b5b2175ecd6981642f4f1c9c0826e31ebf
Vikas Rimmalapudi <123369408+Vikascoditas@users.noreply.github.com>
2024-03-06 15:22:59 +0530

    Update settings.py

--- pythonautomation/settings.py
+++ pythonautomation/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = 'django-insecure-k%+_bga9)un5onl-c@xgrsz=3$5p!&1ax1dqvr9#fgm))uul4*
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-CSRF_TRUSTED_ORIGINS = ['https://recording-restore-gpkrdhuhbq-uw.a.run.app']
+CSRF_TRUSTED_ORIGINS = ['https://recording-restore-gpkrdhuhbq-uw.a.run.app','https://restore.connectandsell.com']
+
 
 ALLOWED_HOSTS = ['*']
 
